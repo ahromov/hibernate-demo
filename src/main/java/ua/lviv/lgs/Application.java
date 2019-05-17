930c8dd86bd008a547c10be41a02761f5700694a
Андрій <@DESKTOP-M7IR61T>
2019-05-17 23:01:35 +0300

    init

--- src/main/java/ua/lviv/lgs/Application.java
+++ src/main/java/ua/lviv/lgs/Application.java
@@ -15,7 +15,7 @@ public class Application {
 	public static void main(String[] args) {
 		Configuration configuration = new Configuration().configure();
 		session = configuration.buildSessionFactory().openSession();
-		
+
 		session.beginTransaction();
 
 		for (int i = 0; i <= 4; i++) {
