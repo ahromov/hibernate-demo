603199ef986359986551f8fb7e2c67c45e3fe223
Андрій <@DESKTOP-M7IR61T>
2019-05-20 13:43:04 +0300

    Added new code for example

--- src/main/java/ua/lviv/lgs/HibernateDemoApplication.java
+++ src/main/java/ua/lviv/lgs/HibernateDemoApplication.java
@@ -1,5 +1,10 @@
 package ua.lviv.lgs;
 
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+
 import org.hibernate.Session;
 import org.hibernate.cfg.Configuration;
 
@@ -15,26 +20,50 @@ public class HibernateDemoApplication {
 		session = configuration.buildSessionFactory().openSession();
 
 		session.beginTransaction();
-		
 		Cart cart1 = new Cart("New Cart");
-
 		session.save(cart1);
 		session.save(new Item(2, cart1));
 		session.save(new Item(2, cart1));
-
 		session.getTransaction().commit();
 
 		session.beginTransaction();
-		
 		Cart cart2 = new Cart("New Cart");
-
 		session.save(cart2);
 		session.save(new Item(2, cart2));
 		session.save(new Item(2, cart2));
+		session.getTransaction().commit();
 
-		session.getTransaction().commit();		
+		session.beginTransaction();
+		List<Cart> carts = loadAllData(Cart.class, session);
+		session.getTransaction().commit();
+		carts.forEach(cart -> System.out.println(cart.getName()));
+
+		cart1.setName("NEW NAME");
 		
+		session.beginTransaction();
+		session.saveOrUpdate(cart1);
+		List<Cart> carts1 = loadAllData(Cart.class, session);
+		session.getTransaction().commit();
+		
+		carts1.forEach(cart -> System.out.println(cart.getName()));
+
+		session.beginTransaction();
+		session.delete(cart1);
+		session.getTransaction().commit();
+		
+		List<Cart> carts2 = loadAllData(Cart.class, session);
+		carts2.forEach(cart -> System.out.println(cart.getName()));
+
 		session.close();
+	}
+
+	private static <T> List<T> loadAllData(Class<T> type, Session session) {
+		CriteriaBuilder builder = session.getCriteriaBuilder();
+		CriteriaQuery<T> criteria = builder.createQuery(type);
+		criteria.from(type);
+		List<T> data = session.createQuery(criteria).getResultList();
+
+		return data;
 	}
 
 }
